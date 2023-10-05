309388edfeee9f6763c71b5c3a268dec7ce3654a
chasereske <chasereske@gmail.com>
2023-10-05 19:10:36 -0400

    Pushed imports to the top and created a line plot

--- Netflix_Office_Project/netflix_office_series_data_project.py
+++ Netflix_Office_Project/netflix_office_series_data_project.py
@@ -1,3 +1,9 @@
+# Imports
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
 # Create the years and duration lists
 years = [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020]
 durations = [103, 101, 99, 100, 100, 95, 95, 96, 93, 90]
@@ -8,11 +14,20 @@ movie_dict = {"years": years, "durations": durations}
 # Print the dictionary
 print(movie_dict)
 
-# Import pandas under its usual alias
-import pandas as pd
-
 # Create a DataFrame from the dictionary
 durations_df = pd.DataFrame(movie_dict)
 
 # Print the DataFrame
 print(durations_df)
+
+# Create a figure using matplotlib.pyplot
+fig = plt.figure()
+
+# Draw a line plot of release_years and durations
+plt.plot(durations_df["years"], durations_df['durations'])
+
+# Create a title
+plt.title("Netflix Movie Durations 2011-2020")
+
+# Show the plot
+plt.show()
