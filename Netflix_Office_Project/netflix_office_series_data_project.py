f573f3cf08bf8bbb587f74a6a00adf37503f4e89
chasereske <chasereske@gmail.com>
2023-10-05 19:36:20 -0400

    Created a color plot of the information

--- Netflix_Office_Project/netflix_office_series_data_project.py
+++ Netflix_Office_Project/netflix_office_series_data_project.py
@@ -83,3 +83,19 @@ for number, cat in netflix_movies_col_subset.iterrows():
 
 # Inspect the first 10 values in your list
 print(colors[10])
+
+# Set the figure style and initalize a new figure
+plt.style.use('fivethirtyeight')
+fig = plt.figure(figsize=(12, 8))
+
+# Create a scatter plot of duration versus release_year
+plt.scatter(netflix_movies_col_subset['release_year'],
+            netflix_movies_col_subset['duration'], color=colors)
+
+# Create a title and axis labels
+plt.title("Movie duration by year of release")
+plt.xlabel("Release year")
+plt.ylabel("Duration (min)")
+
+# Show the plot
+plt.show()
